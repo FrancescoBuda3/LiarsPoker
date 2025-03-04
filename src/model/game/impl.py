25034380da80b23af680f085914f4dad6fc59d60
Francesco Buda <francesco.buda3@studio.unibo.it>
2025-03-04 17:22:54 +0100

    refactor: update GameImpl to use DeckImpl and test refactoring

--- src/model/game/impl.py
+++ src/model/game/impl.py
@@ -1,27 +1,31 @@
 from ..game import *
 from src.utils.debug.impl import Debuggable
-from src.model.deck import generate_deck
+from src.model.deck.impl import DeckImpl
 from src.model.player import Player
 
 
 class GameImpl(Game, Debuggable):
+    STARTING_TURN = 0
+    STARTING_CARDS = 1
+    MINIMUM_PLAYERS = 2
+
     def __init__(self, debug: bool = True):
         Debuggable.__init__(self, debug)
         self.players = []
-        self.deck = generate_deck()
-        self.turn = 0
+        self.deck = DeckImpl()
+        self.turn = self.STARTING_TURN
 
     def addPlayer(self, player:Player):
-       player.cardsInHand = 1
+       player.cardsInHand = self.STARTING_CARDS
        self.players.append(player)
 
     def startTurn(self) -> None:
-        if len(self.players) < 2:
-            raise ValueError("Cannot start without players")
+        if len(self.players) < self.MINIMUM_PLAYERS:
+            raise ValueError("Cannot start without enough players")
         else :
             hands = self.deck.shuffle(player.cardsInHand for player in self.players)
             for i, hand in enumerate(hands):
-                self.players[i] = hand
+                self.players[i].cards = hand
 
     
 
--- test/model/game/game_test.py
+++ test/model/game/game_test.py
@@ -6,24 +6,24 @@ from src.model.game.impl import GameImpl
 class TestGameImpl(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.testPlayer1 = Player("Bob", [], 0)
-        cls.testPlayer2 = Player("Lisa", [], 0)
-        cls.starting_hand_size = 1
+        cls.TEST_PLAYER_1 = Player("Bob", [], 0)
+        cls.TEST_PLAYER_2 = Player("Lisa", [], 0)
 
     def setUp(self):
         self.game = GameImpl()
     
-    def test_doesnt_start_without_players(self):
+    def test_cannot_start_without_enough_players(self):
+        self.game.addPlayer(self.TEST_PLAYER_1)
         with self.assertRaises(ValueError) as context:
             self.game.startTurn()
-        self.assertEqual(str(context.exception), "Cannot start without players")
+        self.assertEqual(str(context.exception), "Cannot start without enough players")
     
     def test_players_have_one_card_at_the_beginning(self):
-        self.game.addPlayer(self.testPlayer1)
-        self.game.addPlayer(self.testPlayer2)
+        self.game.addPlayer(self.TEST_PLAYER_1)
+        self.game.addPlayer(self.TEST_PLAYER_2)
         self.game.startTurn()
-        self.assertEqual(self.starting_hand_size, self.testPlayer1.cardsInHand)
-        self.assertEqual(self.starting_hand_size, self.testPlayer2.cardsInHand)
+        self.assertEqual(self.game.STARTING_CARDS, self.TEST_PLAYER_1.cardsInHand)
+        self.assertEqual(self.game.STARTING_CARDS, self.TEST_PLAYER_2.cardsInHand)
 
 if __name__ == "__main__":
     unittest.main()
