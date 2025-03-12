bf861c7f6f3ffa47cf6bac2f72255751149a8fb9
sevetom <sevetom2611@libero.it>
2025-03-12 18:47:07 +0100

    Add method that returns the lowest possible stake that can be called in StakeHandler

--- src/model/stake/__init__.py
+++ src/model/stake/__init__.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from enum import Enum
 from typing import Protocol
 
 from src.model.deck import Card
@@ -13,34 +14,61 @@ class Stake:
     
     A stake is a combination of cards and a list of ranks.
     """
-    ranks: list[int]
+    ranks: set[int]
     combo: Combination
+    
+class LowestStake(Enum):
+    """
+    Enum class that represents the possible lowest combinations of cards in a poker game.
+    Each value is mapped to the lowest stake that represent the lowest combination.
+    """
+    HIGH_CARD = Stake([1], Combination.HIGH_CARD)
+    PAIR = Stake([1], Combination.PAIR)
+    TWO_PAIR = Stake([1, 1], Combination.TWO_PAIR)
+    THREE_OF_A_KIND = Stake([1], Combination.THREE_OF_A_KIND)
+    STRAIGHT = Stake([1, 2, 3, 4, 5], Combination.STRAIGHT)
+    FLUSH = Stake([], Combination.FLUSH)
+    FULL_HOUSE = Stake([1, 2], Combination.FULL_HOUSE)
+    FOUR_OF_A_KIND = Stake([1], Combination.FOUR_OF_A_KIND)
+    STRAIGHT_FLUSH = Stake([1, 2, 3, 4, 5], Combination.STRAIGHT_FLUSH)
+    ROYAL_FLUSH = Stake([10, 11, 12, 13, 14], Combination.ROYAL_FLUSH)
 
 
 class StakeHandler(Protocol):
-    def get_stake(self) -> Stake:
+        
+    def reset_stake(self) -> None:
         """
-        Returns the current stake, if not set raises an error.
-        Returns:
-            Stake: the current stake
-        """
-        ...
-    
-    def set_stake(self, stake: Stake) -> None:
-        """
-        Sets the given stake as the current stake.
-        Args:
-            stake (Stake): a stake called by a player
+        Resets the current stake.
         """
         ...
 
-    def check_cards(self, cards: list[Card]) -> bool:
+    def check_cards(self, cards: set[Card]) -> bool:
         """
         Checks if the given cards contain the valid combination for the current stake.
         Args:
-            cards (list[Card]): the cards to check
+            cards (set[Card]): the cards to check
 
         Returns:
             bool: true if the cards are valid, false otherwise
+        """
+        ...
+        
+    def get_lowest_next_stake(self) -> Stake:
+        """
+        Returns the lowest possible stake that can be called.
+        If the stake can't be raised higher than None is returned.
+        Returns:
+            Stake: the lowest stake
+        """
+        ...
+        
+    def check_raise(self, stake: Stake) -> bool:
+        """
+        Checks if the given stake is a valid raise.
+        Args:
+            stake (Stake): the stake to check
+
+        Returns:
+            bool: true if the stake is a valid raise, false otherwise
         """
         ...
--- test/model/stake/stake_handler_test.py
+++ test/model/stake/stake_handler_test.py
@@ -9,8 +9,10 @@ from src.model.stake.impl import StakeHandlerImpl
 class TestStakeHandlerImpl(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.TEST_STAKE = Stake([], Combination.HIGH_CARD)
+        cls.TEST_STAKE = Stake([1], Combination.HIGH_CARD)
+        cls.TEST_NEXT_STAKE = Stake([2], Combination.HIGH_CARD)
         cls.TEST_TWO_PAIR = Stake([2, 5], Combination.TWO_PAIR)
+        cls.TEST_NEXT_TWO_PAIR = Stake([5, 3], Combination.TWO_PAIR)
         cls.TEST_TWO_PAIR_CARDS = [Card(Suit.SPADES, 2), Card(Suit.HEARTS, 2), Card(Suit.SPADES, 5), Card(Suit.HEARTS, 5)]
         cls.TEST_INVALID_TWO_PAIR_CARDS = [Card(Suit.SPADES, 2), Card(Suit.HEARTS, 5), Card(Suit.SPADES, 5)]
         cls.TEST_ROYAL_FLUSH = Stake([], Combination.ROYAL_FLUSH)
@@ -20,29 +22,51 @@ class TestStakeHandlerImpl(unittest.TestCase):
     def setUp(self):
         self.stakeHanlder = StakeHandlerImpl()
         
-    def test_get_stake(self):
-        self.stakeHanlder.set_stake(self.TEST_STAKE)
-        self.assertEqual(self.stakeHanlder.get_stake(), self.TEST_STAKE)
+    def test_stake(self):
+        self.stakeHanlder.stake = self.TEST_STAKE
+        self.assertEqual(self.stakeHanlder.stake, self.TEST_STAKE)
+        
+    def test_reset_stake(self):
+        self.stakeHanlder.stake = self.TEST_STAKE
+        self.stakeHanlder.reset_stake()
+        with self.assertRaises(ValueError):
+            self.stakeHanlder.stake
         
     def test_stake_not_set(self):
         with self.assertRaises(ValueError):
-            self.stakeHanlder.get_stake()
+            self.stakeHanlder.stake
+        
+    def test_check_state_not_set(self):
+        with self.assertRaises(ValueError):
+            self.stakeHanlder.check_cards([])
             
     def test_check_cards_two_pair(self):
-        self.stakeHanlder.set_stake(self.TEST_TWO_PAIR)
+        self.stakeHanlder.stake = self.TEST_TWO_PAIR
         self.assertTrue(self.stakeHanlder.check_cards(self.TEST_TWO_PAIR_CARDS))
         
     def test_check_invalid_cards_two_pair(self):
-        self.stakeHanlder.set_stake(self.TEST_TWO_PAIR)
+        self.stakeHanlder.stake = self.TEST_TWO_PAIR
         self.assertFalse(self.stakeHanlder.check_cards(self.TEST_INVALID_TWO_PAIR_CARDS))
             
     def test_check_cards_royal_flush(self):
-        self.stakeHanlder.set_stake(self.TEST_ROYAL_FLUSH)
+        self.stakeHanlder.stake = self.TEST_ROYAL_FLUSH
         self.assertTrue(self.stakeHanlder.check_cards(self.TEST_ROYAL_FLUSH_CARDS))
         
     def test_check__invalid_cards_royal_flush(self):
-        self.stakeHanlder.set_stake(self.TEST_ROYAL_FLUSH)
+        self.stakeHanlder.stake = self.TEST_ROYAL_FLUSH
         self.assertFalse(self.stakeHanlder.check_cards(self.TEST_INVALID_ROYAL_FLUSH_CARDS))
+        
+    def test_get_lowest_next_stake(self):
+        self.stakeHanlder.stake = self.TEST_STAKE
+        self.assertEqual(self.stakeHanlder.get_lowest_next_stake(), self.TEST_NEXT_STAKE)
+        
+    def test_get_lowest_next_stake_two_pair(self):
+        self.stakeHanlder.stake = self.TEST_TWO_PAIR
+        self.assertEqual(self.stakeHanlder.get_lowest_next_stake(), self.TEST_NEXT_TWO_PAIR)
+        
+    def test_get_lowest_next_stake_royal_flush(self):
+        self.stakeHanlder.stake = self.TEST_ROYAL_FLUSH
+        self.assertEqual(self.stakeHanlder.get_lowest_next_stake(), None)
 
 
 if __name__ == "__main__":
