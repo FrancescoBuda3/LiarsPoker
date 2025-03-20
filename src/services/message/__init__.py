34f1659ff7a48d1babe459019af548682229457a
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2025-03-20 12:07:11 +0100

    add enum for message types

--- src/services/message/__init__.py
+++ src/services/message/__init__.py
@@ -1,4 +1,14 @@
 from dataclasses import dataclass
+from enum import Enum
+
+class MessageType(Enum):
+   SART_GAME = "start_game"
+   START_ROUND = "start_round"
+   START_TURN = "start_turn"
+   ROUNT_LOSER = "round_loser"
+   SHOW_CARDS = "show_cards"
+   ELIMINATION = "elimination"
+   GAME_OVER = "game_over"
 
 @dataclass
 class Message:
