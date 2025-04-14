c97b764b4b9c25446abb6ca4c03ab1951cae210e
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2025-04-14 14:12:35 +0200

    fix: update game_loop to use correct Header imports and improve message handling

--- src/controller/game_agent/__init__.py
+++ src/controller/game_agent/__init__.py
@@ -1,17 +1,18 @@
-from email.header import Header
+from src.services.message import Header
 from src.controller.message_factory.impl import MessageFactory
 from src.model.game.GImpl import GameImpl, GamePhase
 from src.model.player import Player
 from src.model.stake import Stake
 from src.model.stake.combination import Combination
-from src.services.connection.impl import ConnectionHandlerImpl
+from src.services.connection.impl import ConnectionHandler
+from src.services.message import Message
 
 
 def game_loop(players: list[Player], id: str):
+    lobby_topic = f"lobby/{id}"
     game = GameImpl()
-    connection_handler = ConnectionHandlerImpl(id)
+    connection_handler = ConnectionHandler(id, [lobby_topic])
     message_factory = MessageFactory()
-    deserializer = Deserializer()
     for p in players:
         game.addPlayer(p)
     game.startGame()
@@ -21,14 +22,16 @@ def game_loop(players: list[Player], id: str):
             connection_handler.send_message(message_factory.create_start_round_message(game.get_players()))
             connection_handler.send_message(message_factory.create_start_turn_message(game.get_current_player(), None))
             
-        msg = connection_handler.wait_message()
+        msg: Message = connection_handler.wait_message(lobby_topic)
         
         match msg.header :
-            case Header.RaiseStake:
-                game.raise_stake(msg.body)
-                connection_handler.send_message(message_factory.create_raise_stake_message(msg.body))
+            case Header.RAISE_STAKE:
+                next_min_stake = game.raise_stake(msg.body)
+                connection_handler.send_message(message_factory.create_start_turn_message(game.get_current_player(), next_min_stake))
                 
-            case Header.CheckLiar:
-                loser_player = game.check_liar()
-                connection_handler.send_message(message_factory.create_check_liar_message())
+            case Header.CHECK_LIAR:
+                loser_player: Player = game.check_liar()
+                connection_handler.send_message(message_factory.create_round_loser_message(loser_player))
+                if not loser_player in game.get_players():
+                    connection_handler.send_message(message_factory.create_elimination_message(loser_player))
                 
