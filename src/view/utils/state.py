1ff77d99b4c42c82d31175aa9944dafa1302583d
Francesco Buda <francesco.buda3@studio.unibo.it>
2025-04-22 15:27:22 +0200

    refactor: simplify connection and user state initialization by removing CLIENT_ID dependency

--- src/view/utils/state.py
+++ src/view/utils/state.py
@@ -4,7 +4,6 @@ import uuid
 
 class __UserState:
     def __init__(self, sessionId=None):
-        self.sessionId = None
         self.username = None
         self.selected_lobby = None
         self.id = uuid.uuid4()
@@ -12,5 +11,4 @@ class __UserState:
     def __str__(self):
         return f"username={self.username}, selected_lobby={self.selected_lobby}"
 
-client_id = os.getenv('CLIENT_ID', 'FixedName')
-user_state = __UserState(sessionId=client_id)
+user_state = __UserState()
