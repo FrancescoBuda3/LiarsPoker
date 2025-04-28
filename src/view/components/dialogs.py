984ffe223d3769fab2f2919e030acd2407329202
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2025-04-28 15:47:28 +0200

    refactor: add limitations in cards dialog

--- src/view/components/dialogs.py
+++ src/view/components/dialogs.py
@@ -5,25 +5,31 @@ from src.model.card.rank import Rank
 from src.model.card.suit import Suit
 from src.model.stake.combination import Combination
 
+
 def combination_picker(minimum_combo: Combination = Combination.HIGH_CARD) -> ui.dialog:
     with ui.dialog() as combination_dialog, ui.card():
         with ui.column().classes('q-pa-sm'):
             ui.label('Select Combination').classes('text-lg')
             for combo in Combination:
                 bt = ui.button(
-                        str(combo),
-                        on_click=lambda c=combo: combination_dialog.submit(str(c)),
-                        color='primary'
-                    ).style('width: 100%').classes('q-mt-xs')
+                    str(combo),
+                    on_click=lambda c=combo: combination_dialog.submit(str(c)),
+                    color='primary'
+                ).style('width: 100%').classes('q-mt-xs')
                 if combo.value < minimum_combo.value:
                     bt.disable()
-                
+
     return combination_dialog
 
-def cards_picker(max_cards: int = 5) -> ui.dialog:
+
+def cards_picker(max_cards: int = 5,
+                 suits: set[Suit] = {Suit.CLUBS,
+                                     Suit.DIAMONDS, Suit.HEARTS, Suit.SPADES},
+                 min_rank: Rank = Rank.ONE,
+                 ) -> ui.dialog:
     cards_selected: list[Card] = []
     card_to_box: dict[Card, ui.checkbox] = {}
-    
+
     def __on_checkbox_change(card):
         if card in cards_selected:
             cards_selected.remove(card)
@@ -33,40 +39,43 @@ def cards_picker(max_cards: int = 5) -> ui.dialog:
             card_to_box[card].value = True
         else:
             card_to_box[card].value = False
-            
+
     def __on_submit() -> list[Card]:
         ret = cards_selected.copy()
         cards_selected.clear()
         for box in card_to_box.values():
             box.clear()
         cards_dialog.submit(ret)
-        
+
     with ui.dialog() as cards_dialog, ui.card():
         with ui.column().classes('items-center'):
             ui.label('Select cards').classes('text-lg')
-            for suit in Suit:
+            for suit in suits:
                 with ui.row():
-                    for rank in Rank:
+                    for i in range(min_rank.value, len(Rank)):
+                        rank = Rank(i)
                         card = Card(suit, rank)
                         with ui.column().classes('items-center'):
                             ui.image(f"static/{card.rank}_of_{card.suit}.png",
-                            ).style('width: 5rem; height: auto')
+                                     ).style('width: 5rem; height: auto')
                             checkbox = ui.checkbox(
                                 value=False,
-                                on_change=lambda c=card: __on_checkbox_change(c),
+                                on_change=lambda c=card: __on_checkbox_change(
+                                    c),
                             )
                             card_to_box[card] = checkbox
             ui.button(
                 'Submit',
                 on_click=lambda: __on_submit()
             )
-        
+
     return cards_dialog
+
 
 def cards_display(cards: list[Card]) -> ui.dialog:
     with ui.dialog() as display_dialog, ui.card():
         for card in cards:
             ui.image(f"static/{card.rank}_of_{card.suit}.png"
-                ).style('width: 5rem; height: auto')
-            
+                     ).style('width: 5rem; height: auto')
+
     return display_dialog
